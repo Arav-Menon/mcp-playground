6e02b60f723762616dbaea45674fe0fb769ed536
Arav menon <aravmenon.ak@gmail.com>
2026-04-10 22:11:19 +0530

    feat : setup client calls

--- client.ts
+++ client.ts
@@ -1,3 +1,4 @@
+import { select } from "@inquirer/prompts";
 import { Client } from "@modelcontextprotocol/sdk/client";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio";
 
@@ -12,13 +13,25 @@ const transport = new StdioClientTransport({
 });
 
 async function main() {
-    await mcp.connect(transport)
-    const [{ tools }, { prompts }, { resources }, { ResourceTemplates }] = await Promise.all([
-        mcp.listTools(),
-        mcp.listPrompts(),
-        mcp.listResources(),
-        mcp.listResourceTemplates()
-    ])
+  await mcp.connect(transport);
+  const [{ tools }, { prompts }, { resources }, { ResourceTemplates }] =
+    await Promise.all([
+      mcp.listTools(),
+      mcp.listPrompts(),
+      mcp.listResources(),
+      mcp.listResourceTemplates(),
+    ]);
+  console.log("You're connected");
+
+  while (true) {
+    const toolName = await select({
+      message: "What would you like to do",
+      choices: ["Query", "tools", "Resources", "Prompts"],
+    });
+  }
+
+  switch(option)
+
 }
 
-main()
+main();
